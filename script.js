7540703ca842d47f547a8ba967a3f561ddce5901
John Bush <silverleaf3260@gmail.com>
2023-11-01 03:30:18 -0700

    basic functionality in place

--- script.js
+++ script.js
@@ -1,4 +1,48 @@
-// switchStyle(theme) {
-//   localStorage.setItem({"theme" , theme);
-// }
-localStorage.setItem("theme" , "blue");
+
+
+$( document ).ready(function() {
+  // localStorage.clear();
+  console.log( localStorage.getItem("theme") )
+  
+
+});
+
+let themeValue = ( localStorage.getItem("theme") )
+  
+if (themeValue === null)  {
+  $(`body`).addClass( "blue" );
+} 
+if (themeValue === "blue")  {
+  $(`body`).addClass( "blue" );
+} 
+if (themeValue === "pink")  {
+  $(`body`).addClass( "pink" );
+} 
+if (themeValue === "green")  {
+  $(`body`).addClass( "green" );
+} 
+  
+
+$("#blue").click(function() {
+  localStorage.setItem("theme" , "blue");
+  console.log("new theme: "+localStorage.getItem("theme"));
+  $(`body`).removeClass();
+  $(`body`).addClass( "blue" );
+})
+
+$("#pink").click(function() {
+  localStorage.setItem("theme" , "pink");
+  console.log("new theme: "+localStorage.getItem("theme"));
+  $(`body`).removeClass();
+  $(`body`).addClass( "pink" );
+})
+
+$("#green").click(function() {
+  localStorage.setItem("theme" , "green");
+  console.log("new theme: "+localStorage.getItem("theme"));
+  $(`body`).removeClass();
+  $(`body`).addClass( "green" );
+
+})
+
+
