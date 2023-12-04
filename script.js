b0bebec679abdd56a61524cce16b17cc74df9b8a
John Bush <142678424+napalmtango@users.noreply.github.com>
2023-12-03 20:11:01 -0800

    Updated theme names

--- script.js
+++ script.js
@@ -1,48 +1,62 @@
-
-
-$( document ).ready(function() {
+$(document).ready(function () {
   // localStorage.clear();
-  console.log( localStorage.getItem("theme") )
-  
-
+  console.log(localStorage.getItem("theme"));
 });
 
-let themeValue = ( localStorage.getItem("theme") )
-  
-if (themeValue === null)  {
-  $(`body`).addClass( "blue" );
-} 
-if (themeValue === "blue")  {
-  $(`body`).addClass( "blue" );
-} 
-if (themeValue === "pink")  {
-  $(`body`).addClass( "pink" );
-} 
-if (themeValue === "green")  {
-  $(`body`).addClass( "green" );
-} 
-  
+let themeValue = localStorage.getItem("theme");
 
-$("#blue").click(function() {
-  localStorage.setItem("theme" , "blue");
-  console.log("new theme: "+localStorage.getItem("theme"));
-  $(`body`).removeClass();
-  $(`body`).addClass( "blue" );
-})
+if (themeValue === null) {
+  $(`body`).addClass("pigskin");
+  $(`h1`).addClass("h1pigskin");
+  $(`h2`).addClass("h2pigskin");
+}
+if (themeValue === "pigskin") {
+  $(`body`).addClass("pigskin");
+  $(`h1`).addClass("h1pigskin");
+  $(`h2`).addClass("h2pigskin");
+}
 
-$("#pink").click(function() {
-  localStorage.setItem("theme" , "pink");
-  console.log("new theme: "+localStorage.getItem("theme"));
-  $(`body`).removeClass();
-  $(`body`).addClass( "pink" );
-})
+if (themeValue === "nfl") {
+  $(`body`).addClass("nfl");
+  $(`h1`).addClass("h1nfl");
+  $(`h2`).addClass("h2nfl");
+}
+if (themeValue === "turf") {
+  $(`body`).addClass("turf");
+  $(`h1`).addClass("h1turf");
+  $(`h2`).addClass("h2turf");
+}
 
-$("#green").click(function() {
-  localStorage.setItem("theme" , "green");
-  console.log("new theme: "+localStorage.getItem("theme"));
-  $(`body`).removeClass();
-  $(`body`).addClass( "green" );
+// buttons
+$("#pigskin").click(function () {
+  localStorage.setItem("theme", "pigskin");
+  console.log("new theme: " + localStorage.getItem("theme"));
+  $(`body`).removeClass("pigskin nfl turf");
+  $(`h1`).removeClass("h1pigskin h1nfl h1turf");
+  $(`h2`).removeClass("h2pigskin h2nfl h2turf");
+  $(`body`).addClass("pigskin");
+  $(`h1`).addClass("h1pigskin");
+  $(`h2`).addClass("h2pigskin");
+});
 
-})
+$("#nfl").click(function () {
+  localStorage.setItem("theme", "nfl");
+  console.log("new theme: " + localStorage.getItem("theme"));
+  $(`body`).removeClass("pigskin nfl turf");
+  $(`h1`).removeClass("h1pigskin h1nfl h1turf");
+  $(`h2`).removeClass("h2pigskin h2nfl h2turf");
+  $(`body`).addClass("nfl");
+  $(`h1`).addClass("h1nfl");
+  $(`h2`).addClass("h2nfl");
+});
 
-
+$("#turf").click(function () {
+  localStorage.setItem("theme", "turf");
+  console.log("new theme: " + localStorage.getItem("theme"));
+  $(`body`).removeClass("pigskin nfl turf");
+  $(`h1`).removeClass("h1pigskin h1nfl h1turf");
+  $(`h2`).removeClass("h2pigskin h1nfl h2turf");
+  $(`body`).addClass("turf");
+  $(`h1`).addClass("h1turf");
+  $(`h2`).addClass("h2turf");
+});
